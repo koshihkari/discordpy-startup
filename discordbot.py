f076daa9b6d766b1e5682a0b0d5c3dc080bb7ff0
koshihkari <60177769+koshihkari@users.noreply.github.com>
2020-08-01 21:11:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,48 @@
-from discord.ext import commands
-import os
 import traceback
+import time
+import os
+import random
 
-bot = commands.Bot(command_prefix='/')
+import asyncio
+import discord
+from discord.ext import commands
+
+
 token = os.environ['DISCORD_BOT_TOKEN']
 
+# 読み込むコグの名前を格納しておく。
+INITIAL_EXTENSIONS = [
+    'cog.search',
+    'cog.game'
+    ]
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# クラスの定義。ClientのサブクラスであるBotクラスを継承。
+class MyBot(commands.Bot):
+
+    # MyBotのコンストラクタ。
+    def __init__(self, command_prefix):
+        # スーパークラスのコンストラクタに値を渡して実行。
+        super().__init__(command_prefix)
+
+        # INITIAL_COGSに格納されている名前から、コグを読み込む。
+        # エラーが発生した場合は、エラー内容を表示。
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+
+    # Botの準備完了時に呼び出されるイベント
+    async def on_ready(self):
+        print('-----')
+        print(self.user.name)
+        print(self.user.id)
+        print('-----')
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
 
 
-bot.run(token)
+# MyBotのインスタンス化及び起動処理。
+if __name__ == '__main__':
+    bot = MyBot(command_prefix='==')
+    bot.run(token)
