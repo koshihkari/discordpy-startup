1ef01fadd9eb64e9adc98094d5d25fede0bd6965
koshihkari <60177769+koshihkari@users.noreply.github.com>
2020-08-10 18:29:42 +0900

    Update game.py

--- cog/game.py
+++ cog/game.py
@@ -5,6 +5,8 @@ import discord
 from discord.ext import commands
 
 
+OWN_ID = 668825221377294347
+
 class Numeron(object):
     def __init__(self,player_1,player_2):
         self.player_1 = player_1
@@ -14,6 +16,36 @@ class Numeron(object):
         self.situation_p1 = []
         self.situation_p2 = []
         self.now_player = player_1
+        #以下はbot対戦時のみ
+        self.candidates = [(x,y,z) for x in range(10) for y in range(10) for z in range(10) if x != y and y != z and x != z]
+        self.new_candidates = []
+        self.bot_eat = 0
+        self.bot_bite = 0
+        self.bot_number = 0
+
+    def start(self):
+        """
+        botの初手数字決定
+        """
+        l = [(x,y,z) for x in range(10) for y in range(10) for z in range(10) if x != y and y != z and x != z]
+        number = random.choice(l)
+        return number
+
+    def ai_decide(self):
+        for cand in self.candidates:
+            num_of_eat = 0
+            num_of_bite = 0
+            for number_digit, cand_digit in zip(self.bot_number,cand):
+                if cand_digit == number_digit:
+                    num_of_eat += 1  # 同じ位置に同じ数字がある、EAT
+                elif cand_digit in self.bot_number:
+                    num_of_bite += 1  # 同じ位置ではないが同じ数字がある、BITE
+            if num_of_eat == self.bot_eat and num_of_bite == self.bot_bite:
+                self.new_candidates.append(cand)  # EAT、BITEの条件が適合したので新しい候補リストに入れる
+        self.candidates = self.new_candidates.copy()
+        self.new_candidates.clear()
+        atack = random.choice(self.candidates)
+        return atack
 
     def judge(self,number):
         """
@@ -30,7 +62,7 @@ class Numeron(object):
             return False
         return base_list
 
-    def eat_bite(self,atack_num:list,opponent_num:list):
+    def eat_bite(self,atack_num,opponent_num):
         """
         EAT,BITEの判定をします。
         """
@@ -40,6 +72,7 @@ class Numeron(object):
         for _ in range(3):
             use_judge = [0,1,2]
             use_judge.remove(n)
+            print(opponent_num,atack_num,n)
             if opponent_num[n] != atack_num[n]:
                 if opponent_num[n] == atack_num[use_judge[0]] or opponent_num[n] == atack_num[use_judge[1]]:
                     bite += 1
@@ -65,8 +98,9 @@ class Numeron(object):
             embed.add_field(name=self.player_2.name,value=reply2,inline=False)
         return embed
 
-    def rt_dict(self,answer,eat,bite):
-        d = {'ans': answer, 'eat': eat, 'bite': bite}
+    def rt_dict(self,atack_list,eat,bite):
+        atack_list_str = ''.join(map(str, atack_list))
+        d = {'ans': atack_list_str, 'eat': eat, 'bite': bite}
         return d
 
 
@@ -83,10 +117,14 @@ class GmaeCog(commands.Cog):
             await ctx.send('DMではなくサーバーのチャンネルでコマンドを実行してください')
             return
         if opponent.bot:
-            await ctx.send('botをメンションしないでください')
-            return
+            if opponent.id != OWN_ID:
+                await ctx.send(f'{self.bot.user.name}以外のbotをメンションしないでください')
+                return
         random_number = random.randint(1,2)
-        if random_number == 1:
+        if opponent.id == OWN_ID:
+            await ctx.send(f'{ctx.author.name}が先攻です')
+            numeron = Numeron(ctx.author,opponent)
+        elif random_number == 1:
             reply_atack = ctx.author.name + "が先攻です"
             await ctx.send(reply_atack)
             numeron = Numeron(ctx.author,opponent)
@@ -96,79 +134,114 @@ class GmaeCog(commands.Cog):
             numeron = Numeron(opponent,ctx.author)
         #このループで数字を決める
         while True:
-            #個人チャットでの受け取りをできるようにする
-            def check_player(m):
-                return m.author == numeron.player_1 and m.channel == numeron.player_1.dm_channel
-            def check_opponent(m):
-                return m.author == numeron.player_2 and m.channel == numeron.player_2.dm_channel
-
-            await ctx.send('DMでBotの指示を待機してください')
-            if numeron.player_1.dm_channel == None:
-                await numeron.player_1.create_dm()
-            await numeron.player_1.dm_channel.send('決めた数字を送ってください(相手が当てる数字)。')
-            #numeron.judgeを用いて番号を決定する
-            while True:    
+            if numeron.player_2.bot:
+                def check(m):
+                    return m.author == ctx.author and m.channel == ctx.channel
+                await ctx.send('決めた数字を送ってください(相手が当てる数字)。')
                 try:
-                    str_num1 = await self.bot.wait_for('message',check=check_player,timeout=120.0)
+                    str_num1 = await self.bot.wait_for('message',check=check,timeout=30.0)
                 except asyncio.TimeoutError:
-                    await ctx.send('2分間の無操作によりゲームを終了します')
+                    await ctx.send('30秒間の無操作によりゲームを終了します')
                     return
                 if not numeron.judge(str_num1.content):
                     continue
                 else:
+                    numeron.player_1_number = numeron.judge(str_num1.content)
+                    numeron.player_2_number = numeron.start()
                     break
-            
-            if numeron.player_2.dm_channel == None:
-                await numeron.player_2.create_dm()
-            await numeron.player_2.dm_channel.send('決めたを数字を送ってください(相手が当てる数字)。')            
-            while True:
-                try:
-                    str_num2 = await self.bot.wait_for('message',check=check_opponent,timeout=120.0)
-                except asyncio.TimeoutError:
-                    await ctx.send('2分間の無操作によりゲームを終了します')
-                    return
-                if not numeron.judge(str_num2.content):
-                    continue
-                else:
-                    break
+            else:
+                #個人チャットでの受け取りをできるようにする
+                def check_player(m):
+                    return m.author == numeron.player_1 and m.channel == numeron.player_1.dm_channel
+                def check_opponent(m):
+                    return m.author == numeron.player_2 and m.channel == numeron.player_2.dm_channel
 
-            numeron.player_1_number = numeron.judge(str_num1.content)
-            numeron.player_2_number = numeron.judge(str_num2.content)
-            break
+                await ctx.send('DMでBotの指示を待機してください')
+                if numeron.player_1.dm_channel == None:
+                    await numeron.player_1.create_dm()
+                await numeron.player_1.dm_channel.send('決めた数字を送ってください(相手が当てる数字)。')
+                #numeron.judgeを用いて番号を決定する
+                while True:    
+                    try:
+                        str_num1 = await self.bot.wait_for('message',check=check_player,timeout=120.0)
+                    except asyncio.TimeoutError:
+                        await ctx.send('2分間の無操作によりゲームを終了します')
+                        return
+                    if not numeron.judge(str_num1.content):
+                        continue
+                    else:
+                        break
+                numeron.player_1_number = numeron.judge(str_num1.content)
+                if numeron.player_2.dm_channel == None:
+                    await numeron.player_2.create_dm()
+                await numeron.player_2.dm_channel.send('決めたを数字を送ってください(相手が当てる数字)。')            
+                while True:
+                    try:
+                        str_num2 = await self.bot.wait_for('message',check=check_opponent,timeout=120.0)
+                    except asyncio.TimeoutError:
+                        await ctx.send('2分間の無操作によりゲームを終了します')
+                        return
+                    if not numeron.judge(str_num2.content):
+                        continue
+                    else:
+                        break
+                numeron.player_2_number = numeron.judge(str_num2.content)
+                break
+
 
         turn_rep = numeron.now_player.mention + 'の番です'
         await ctx.send(turn_rep)
+        n = 1
 
         while True:          
             #攻めの値の決定
-            def check(m):
-                return m.author == numeron.now_player and m.channel == ctx.channel
-            try:
-                atack_num = await self.bot.wait_for('message',check=check,timeout=180.0)
-            except asyncio.TimeoutError:
-                await ctx.send('3分間も放置しやがって！終わるわよ')
-                return
-            if atack_num.content == 'end':
-                await ctx.send('ゲームを終了します。')
-                return
-            #numeron.judgeを活用してatack_numをリスト化する
-            atack_list = numeron.judge(atack_num.content)
-            if not atack_list:
-                continue
+            if numeron.now_player.id == OWN_ID:
+                if n == 2:
+                    atack_list = numeron.start()
+                else:
+                    atack_list = numeron.ai_decide()
+            else:
+                def check_p1_bot(m):
+                    return m.author == numeron.now_player and m.channel == ctx.channel
+                try:
+                    atack_num = await self.bot.wait_for('message',check=check_p1_bot,timeout=180.0)
+                except asyncio.TimeoutError:
+                    await ctx.send('3分間も放置しやがって！終わるわよ')
+                    return
+                if atack_num.content == 'end':
+                    await ctx.send('ゲームを終了します。')
+                    return
+                #numeron.judgeを活用してatack_numをリスト化する
+                atack_list = numeron.judge(atack_num.content)
+                if not atack_list:
+                    continue
             if numeron.now_player == numeron.player_1:
                 eat, bite = numeron.eat_bite(atack_list,numeron.player_2_number)
             else:
                 eat, bite = numeron.eat_bite(atack_list,numeron.player_1_number) 
-            result_dict = numeron.rt_dict(atack_num.content, eat, bite)
+            if numeron.now_player.id == OWN_ID:
+                numeron.bot_eat = eat
+                numeron.bot_bite = bite
+                numeron.bot_number = atack_list
+            result_dict = numeron.rt_dict(atack_list, eat, bite)
             if numeron.now_player == numeron.player_1:
                 numeron.situation_p1.append(result_dict)
             elif numeron.now_player == numeron.player_2:
                 numeron.situation_p2.append(result_dict)
-            embed = numeron.mk_situation(numeron.situation_p1,numeron.situation_p2)
-            await ctx.send(embed=embed)
+            if numeron.player_2.bot:
+                if numeron.now_player.id != OWN_ID:
+                    pass
+                else:
+                    embed = numeron.mk_situation(numeron.situation_p1,numeron.situation_p2)
+                    await ctx.send(embed=embed)
+            else:
+                embed = numeron.mk_situation(numeron.situation_p1,numeron.situation_p2)
+                await ctx.send(embed=embed)
             if eat == 3:
                 finish_rep = numeron.now_player.name + 'の勝利です!'
-                tell_answer = numeron.player_1.name + ": " + str_num1.content + ', ' + numeron.player_2.name + ": " + str_num2.content
+                str_p1_number = ''.join(map(str, numeron.player_1_number))
+                str_p2_number = ''.join(map(str, numeron.player_2_number))
+                tell_answer = numeron.player_1.name + ": " + str_p1_number + ', ' + numeron.player_2.name + ": " + str_p2_number
                 await ctx.send(finish_rep)
                 await ctx.send(tell_answer)
                 break
@@ -177,8 +250,12 @@ class GmaeCog(commands.Cog):
             else:
                 numeron.now_player = numeron.player_1
             
-            turn_rep = numeron.now_player.mention + 'の番です'
-            await ctx.send(turn_rep)
+            if numeron.now_player.bot:
+                pass
+            else:
+                turn_rep = numeron.now_player.mention + 'の番です'
+                await ctx.send(turn_rep)
+            n += 1
 
     @commands.command(aliases=["nr"])
     async def nrule(self,ctx):
